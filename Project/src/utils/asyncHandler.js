6da70ce4c58ba11c824c908524af88dae145e87d
MuhammadUmar969 <muhammadumarazizkhan23@gmail.com>
2024-07-25 16:43:18 +0500

    configure & util code

---
+++ Project/src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => {
+  (res, req, next) => {
+    Promise.resolve(requestHandler(res, req, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = (fn) => async (res, req, next) => {
+//   try {
+//     await fn(res, req, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message || "Internal Server Error",
+//     });
+//   }
+// };
